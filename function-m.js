082d23f34fc45747a0d431deef608b83d6152ec8
amirarabirazdadeh <amirarabi1362@gmail.com>
2023-05-28 12:29:07 +0330

    initial commit14852

---
+++ function-m.js
@@ -0,0 +1,15 @@
+//exports.handler =  async function(event, context) {
+  //console.log("EVENT: \n" + JSON.stringify(event, null, 2))
+  //return "Hello World!!"
+//}
+var url = require('url');
+exports.handler = async function(event,context) {
+  var url_parts = url.parse(context.clientContext.authority, true);
+    var query = url_parts.query;
+    //var path = url_parts.path;
+    return {
+        status: 200,
+        //body: "hello world! " + "EVENT : " + JSON.stringify(event) + "CONTEXT : " + JSON.stringify(context.clientContext),
+        body: "hello " + query.user + "!" 
+    }
+}
