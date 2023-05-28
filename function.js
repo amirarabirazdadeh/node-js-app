b94d562f77c3aa17f4c19cb7e599ae566e0bad93
amirarabirazdadeh <amirarabi1362@gmail.com>
2023-05-28 10:40:04 +0330

    initial commit7

--- function.js
+++ function.js
@@ -1,4 +1,17 @@
-exports.handler =  async function(event, context) {
-  console.log("EVENT: \n" + JSON.stringify(event, null, 2))
-  return "Hello World!!"
+//exports.handler =  async function(event, context) {
+  //console.log("EVENT: \n" + JSON.stringify(event, null, 2))
+  //return "Hello World!!"
+//}
+
+exports.handler = async function(context) {
+    console.log(context.request.headers);
+    var token = context.request.headers['x-internal-token'];
+    console.log("Token presented : ", token);
+
+    // do some authn and authz based on token received
+
+    return {
+        status: 200,
+        body: "hello world! " + "Token presented : ", token
+    }
 }
