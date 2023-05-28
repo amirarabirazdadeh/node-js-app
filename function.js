eaa88a5e5ac2a30b71fafac08c23d8a86a297fb2
amirarabirazdadeh <amirarabi1362@gmail.com>
2023-05-28 10:47:08 +0330

    initial commit8965

--- function.js
+++ function.js
@@ -4,14 +4,8 @@
 //}
 
 exports.handler = async function(context) {
-    console.log(context.request.headers);
-    var token = context.request.headers['x-internal-token'];
-    console.log("Token presented : ", token);
-
-    // do some authn and authz based on token received
-
     return {
         status: 200,
-        body: "hello world! " + "Token presented : ", token
+        body: "hello world! " + "Token presented : ", context
     }
 }
