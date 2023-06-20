512c795fb3425e62183361ae2d9d1ed8968fb725
amirarabirazdadeh <amirarabi1362@gmail.com>
2023-06-20 08:58:31 +0330

    initial commit10000000000000

--- function.js
+++ function.js
@@ -13,9 +13,18 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "handler": () => (/* binding */ handler)
 /* harmony export */ });
 /* harmony import */ var _webiny_handler_aws__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @webiny/handler-aws */ "./packages/handler-aws/dist/index.js");
+/* harmony import */ var fs__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! fs */ "fs");
+/* harmony import */ var fs__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(fs__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var util__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! util */ "util");
+/* harmony import */ var util__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(util__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var stream__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! stream */ "stream");
+/* harmony import */ var stream__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(stream__WEBPACK_IMPORTED_MODULE_3__);
 //import { LambdaResponse } from "@fastify/aws-lambda";
 
 //const { Base64 } = require("js-base64");
+
+
+
 
 const createLambdaEvent = (options = {}) => {
   return {
@@ -60,9 +69,14 @@ const handler = async (event, context) => {
         });
       })]
     });
+    const streamPipeline = (0,util__WEBPACK_IMPORTED_MODULE_2__.promisify)(stream__WEBPACK_IMPORTED_MODULE_3__.pipeline);
     const response = await handler(ev, context);
     //return response.body;
-    return Buffer.from(response.body, 'binary');
+
+    return await streamPipeline(response.body, (0,fs__WEBPACK_IMPORTED_MODULE_1__.createWriteStream)(response.body, {
+      encoding: 'base64'
+    }));
+    //return Buffer.from(response.body, 'binary');
   } catch (err) {
     return {
       statusCode: 500,
