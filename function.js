a9b1c4d6effc37859bfd472299652f33bc6c7a3c
amirarabirazdadeh <amirarabi1362@gmail.com>
2023-06-20 08:44:52 +0330

    initial commit1000000000000

--- function.js
+++ function.js
@@ -61,8 +61,8 @@ const handler = async (event, context) => {
       })]
     });
     const response = await handler(ev, context);
-    return response.body;
-    //return Buffer.from(response.body, 'binary');
+    //return response.body;
+    return Buffer.from(response.body, 'binary');
   } catch (err) {
     return {
       statusCode: 500,
